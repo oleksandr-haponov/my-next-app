f80d0bb5b49fd162a0aa3f100b7583835cfbee96
Oleksandr Haponov <olex.haponov@gmail.com>
2025-07-31 09:46:19 +0300

    start next.js

---
+++ prettier.config.js
@@ -0,0 +1,16 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+  endOfLine: 'lf',
+  jsxSingleQuote: false,
+  embeddedLanguageFormatting: 'auto',
+  proseWrap: 'preserve',
+};
